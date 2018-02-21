cc0e41f0ef4378876a781bdec5f66df9545ebd4e
thatsgaurav87 <gauravthakre87@gmail.com>
2018-02-21 20:57:44 +0530

    updated EC2 creation code

--- terraform/project-ec2/main.tf
+++ terraform/project-ec2/main.tf
@@ -2,28 +2,14 @@
 #I am in terraform
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "ami-66506c1c"
   instance_type = "t2.micro"
+  key_name = "/home/gaurav/IdeaProjects/techops/terraform/project-ec2/Gaurav.key_name"
 
   tags {
     Name = "HelloWorld"
